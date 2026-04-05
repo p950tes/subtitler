3558a42b83831db85aba4e13b07326e066285c91
p950tes <tobiasericsson90@hotmail.com>
2026-04-05 12:50:01 +0200

    Improved SubtitleParser tests

--- src/test/java/se/p950tes/subtitler/tooling/parsing/SubtitleParserTest.java
+++ src/test/java/se/p950tes/subtitler/tooling/parsing/SubtitleParserTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import java.nio.file.Path;
 import java.util.List;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -42,11 +43,7 @@ There is no stopping in the red zone.
 		SubtitleFile result = factory.newParser(file).parse();
 		assertEquals(file, result.getPath());
 		assertEquals(1, result.getEntries().size());
-		
-		SubtitleEntry entry = result.getEntries().get(0);
-		assertEquals(1, entry.getIndex());
-		assertEquals("00:01:33,385 --> 00:01:36,929", entry.getTimestamp());
-		assertEquals(List.of("There is no stopping in the red zone."), entry.getLines());
+		assertEquals(new SubtitleEntry(1, "00:01:33,385 --> 00:01:36,929", List.of("There is no stopping in the red zone.")), result.getEntries().get(0));
 	}
 	
 	@Test
@@ -68,16 +65,113 @@ and unloading of passengers only.
 		
 		List<SubtitleEntry> entries = result.getEntries();
 		assertEquals(2, entries.size());
+		assertEquals(new SubtitleEntry(1, "00:01:33,385 --> 00:01:36,929", List.of("There is no stopping in the red zone.")), result.getEntries().get(0));
+		assertEquals(new SubtitleEntry(2, "00:01:37,014 --> 00:01:42,226", List.of("The white zone is for immediate loading", "and unloading of passengers only.")), result.getEntries().get(1));
+	}
+	
+	@Test
+	void junk_at_beginning() {
+		when(fileManager.readLinesFromFile(file)).thenReturn(listFrom(
+"""
+junk
+1
+00:01:33,385 --> 00:01:36,929
+There is no stopping in the red zone.
+"""));
 		
-		SubtitleEntry entry1 = entries.get(0);
-		assertEquals(1, entry1.getIndex());
-		assertEquals("00:01:33,385 --> 00:01:36,929", entry1.getTimestamp());
-		assertEquals(List.of("There is no stopping in the red zone."), entry1.getLines());
+		SubtitleFile result = factory.newParser(file).parse();
+		assertEquals(file, result.getPath());
+		assertEquals(1, result.getEntries().size());
+		assertEquals(new SubtitleEntry(1, "00:01:33,385 --> 00:01:36,929", List.of("There is no stopping in the red zone.")), result.getEntries().get(0));
+	}
+	
+	@Test
+	void empty_entries() {
+		when(fileManager.readLinesFromFile(file)).thenReturn(listFrom(
+"""
+1
+00:00:01,000 --> 00:00:02,000
+2
+00:00:02,000 --> 00:00:03,000
+There is no stopping in the red zone.
+3
+00:00:03,000 --> 00:00:04,000
+4
+00:00:04,000 --> 00:00:05,000
+The white zone is for immediate loading
+and unloading of passengers only.
+5
+00:00:05,000 --> 00:00:06,000
+"""));
 		
-		SubtitleEntry entry2 = entries.get(1);
-		assertEquals(2, entry2.getIndex());
-		assertEquals("00:01:37,014 --> 00:01:42,226", entry2.getTimestamp());
-		assertEquals(List.of("The white zone is for immediate loading", "and unloading of passengers only."), entry2.getLines());
+		SubtitleFile result = factory.newParser(file).parse();
+		assertEquals(file, result.getPath());
+		List<SubtitleEntry> entries = result.getEntries();
+		assertEquals(5, entries.size());
+		
+		assertEquals(new SubtitleEntry(1, "00:00:01,000 --> 00:00:02,000", List.of()), entries.get(0));
+		assertEquals(new SubtitleEntry(2, "00:00:02,000 --> 00:00:03,000", List.of("There is no stopping in the red zone.")), entries.get(1));
+		assertEquals(new SubtitleEntry(3, "00:00:03,000 --> 00:00:04,000", List.of()), entries.get(2));
+		assertEquals(new SubtitleEntry(4, "00:00:04,000 --> 00:00:05,000", List.of("The white zone is for immediate loading", "and unloading of passengers only.")), entries.get(3));
+		assertEquals(new SubtitleEntry(5, "00:00:05,000 --> 00:00:06,000", List.of()), entries.get(4));
+	}
+	
+	@Test
+	void whitespace() {
+		when(fileManager.readLinesFromFile(file)).thenReturn(listFrom(
+"""
+
+1
+00:00:01,000 --> 00:00:02,000
+
+
+2
+00:00:02,000 --> 00:00:03,000
+There is no stopping in the red zone.
+
+
+3
+00:00:05,000 --> 00:00:06,000
+
+
+
+"""));
+		
+		SubtitleFile result = factory.newParser(file).parse();
+		assertEquals(file, result.getPath());
+		List<SubtitleEntry> entries = result.getEntries();
+		assertEquals(3, entries.size());
+		
+		assertEquals(new SubtitleEntry(1, "00:00:01,000 --> 00:00:02,000", List.of()), entries.get(0));
+		assertEquals(new SubtitleEntry(2, "00:00:02,000 --> 00:00:03,000", List.of("There is no stopping in the red zone.")), entries.get(1));
+		assertEquals(new SubtitleEntry(3, "00:00:05,000 --> 00:00:06,000", List.of()), entries.get(2));
+	}
+	
+	@Test
+	@Disabled("Future improvement")
+	void whitespace_in_single_entry() {
+		when(fileManager.readLinesFromFile(file)).thenReturn(listFrom(
+"""
+
+1
+
+
+00:00:01,000 --> 00:00:02,000
+
+
+The white zone is for immediate loading
+
+
+and unloading of passengers only.
+
+
+"""));
+		SubtitleFile result = factory.newParser(file).parse();
+		assertEquals(file, result.getPath());
+		List<SubtitleEntry> entries = result.getEntries();
+		assertEquals(1, entries.size());
+		
+		assertEquals(new SubtitleEntry(1, "00:00:01,000 --> 00:00:02,000", List.of("The white zone is for immediate loading", "and unloading of passengers only.")), entries.get(0));
 	}
 
 	private static List<String> listFrom(String content) {
